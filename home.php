27e7d94675e40798e550c16405c3fd46cc985c9c
epsilonsynapses <epsilonsynapses@gmail.com>
2018-08-24 15:20:57 +0100

    new login page... again

--- home.php
+++ home.php
@@ -12,20 +12,20 @@ if (!isset($_SESSION["is_phone"])){
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>CyberNetics&trade; | Home</title>
 		<link rel="stylesheet" type="text/css" href="static.css">
-		<link rel="icon" href="png/logo.jpg">
+		<link rel="icon" href="logo.jpg">
 	</head>
 	<body>
 	<?php if ($_SESSION["is_phone"]){ ?>
-		<div style="position: fixed; background-color: #000000; top: 0; left: 0; right: 0; border-bottom: 1px solid #ffffff;">
+		<div class="header">
 			<a href="">
-				<img src="png/logo.jpg" style="width: 35px; height: 35px; float: left;">
-				<h3 style="color: #ffffff; float: left; margin: 7px 0 0 5px;">CyberNetics&trade;</h3>
+				<img src="logo.jpg" class="m_logo">
+				<h3>CyberNetics&trade;</h3>
 			</a>
 		<?php if (isset($_SESSION["is_logged"])){ ?>
-			<img src="png/user.png" style="height: 30px; width: 30px; float: right; margin-top: 3px; margin-right: 2px;">
+			<img id="m_user" src="user.png">
 		<?php } else { ?>
 			<a href="login.php">
-				<img src="png/login.png" style="height: 30px; width: 30px; float: right; margin-top: 3px; margin-right: 2px;">
+				<img id="m_login" src="login.png">
 			</a>
 		<?php } ?>
 		</div>
@@ -36,20 +36,20 @@ if (!isset($_SESSION["is_phone"])){
 		</center>
 	<?php if (isset($_SESSION["is_logged"])){ ?>
 		<a href="logout.php">
-			<img src="png/logout.png" style="position: fixed; bottom: 5px; right: 5px; background-color: #000000; border: 1px solid #ffffff; border-radius: 100%; height: 30px; width: 30px; padding: 3px;">
+			<img src="logout.png" style="position: fixed; bottom: 5px; right: 5px; background-color: #000000; border: 1px solid #ffffff; border-radius: 100%; height: 30px; width: 30px; padding: 3px;">
 		</a>
 	<?php } ?>
 	<?php } else { ?>
 		<div class="header">
 			<a href="" title="CyberNetics&trade; | Home">
-				<img class="logo" src="png/logo.jpg">
+				<img class="logo" src="logo.jpg">
 				<h1>CyberNetics&trade;</h1>
 			</a>
 		<?php if (isset($_SESSION["is_logged"])){ ?>
-			<img src="png/user.png" title="<?php echo $_SESSION["username"]; ?>" style="height: 40px; width: 40px; float: right; margin: 7px 5px 0 5px;">
+			<img src="user.png" title="<?php echo $_SESSION["username"]; ?>" style="height: 40px; width: 40px; float: right; margin: 7px 5px 0 5px;">
 		<?php } else { ?>
-			<a href="login.php" style="float: right;">
-				<img src="png/login.png" style="height: 40px; width: 40px; margin: 7px 5px 0 5px;">
+			<a href="login.php">
+				<img src="login.png" style="height: 40px; width: 40px; margin: 7px 5px 0 5px; float: right;">
 			</a>
 		<?php } ?>
 		</div>
@@ -58,12 +58,11 @@ if (!isset($_SESSION["is_phone"])){
 				<h2 style="cursor: default;">Welcome to CyberNetics&trade; official website</h2>
 			</div>
 		</center>
-		<?php if (isset($_SESSION["is_logged"])){ ?>
-			<a href="logout.php">
-				<img src="png/logout.png" title="Logout" style="position: fixed; bottom: 10px; right: 10px; background-color: #000000; border: 1px solid #ffffff; border-radius: 100%; height: 40px; width: 40px; padding: 3px;">
-			</a>
-		<?php } ?>
+	<?php if (isset($_SESSION["is_logged"])){ ?>
+		<a href="logout.php">
+			<img src="logout.png" title="Logout" style="position: fixed; bottom: 10px; right: 10px; background-color: #000000; border: 1px solid #ffffff; border-radius: 100%; height: 40px; width: 40px; padding: 3px;">
+		</a>
+	<?php } ?>
 	<?php } ?>
 	</body>
 </html>
-	
--- login.php
+++ login.php
@@ -1,46 +1,7 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/loginform.css">
-    </head>
-    
-    <div class="container">
-    <div class="row login block-lg">
-    <div class="col-md-4 col-md-offset-4">    
-    <div class="panel panel-default">
-        <div class="panel-body">
-    <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
-</html>
-
-
-
 <?php
 session_start();
 if (isset($_SESSION["is_logged"])){
-    header("Location: home.php");
+    header("Location: dashboard.php");
     exit();
 }
 
@@ -48,7 +9,6 @@ function clean($data){
     return htmlspecialchars(stripslashes(trim($data)));
 }
 
-$error = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $conn = mysqli_connect("127.0.0.1", "root", "3E9gPzv9TyhyQdBa", "users") or die("Conenction failed");
     $username = clean($_POST["username"]);
@@ -56,16 +16,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $sql = "SELECT uname, password FROM info WHERE (uname='$username' AND password='$password')";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) == 1){
+        print "found";
         $_SESSION["is_logged"] = true;
         $_SESSION["username"] = $username;
-        header("Location: home.php");
+        header("Location: dashboard.php");
     } else {
-        $error = "Wrong username or password";
+        print "not found";
+        $_SESSION["error"] = "Wrong username or password";
         header("Location: login.php");
     }
     mysqli_close($conn);
     exit();
 }
-
-
-
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Commpatible" content="IE=edge, chrome=1">
+        <title>Login</title>
+        <link rel="stylesheet" href="static.css">
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <div class="header">
+			<a href="home.php">
+				<img src="logo.jpg" class="logo">
+				<h1>CyberNetics&trade;</h1>
+			</a>
+            <a href="login.php">
+				<img src="login.png" style="height: 40px; width: 40px; margin: 7px 5px 0 5px; float: right;">
+			</a>
+        </div>
+        <center>
+            <div class="main">
+                <span><?php if (isset($_SESSION["error"])){ echo $_SESSION["error"]; } else { echo ""; } ?></span>
+                <form action="login.php" method="post">
+                    <label for="username">Username:</label><br>
+                    <input type="text" id="username" name="username" required><br>
+                    <label for="password">Password:</label><br>
+                    <input type="password" id="password" name="password" required><br>
+                    <input type="submit" value="Login">
+                </form>
+            </div>
+        </center>
+    </body>
+</html>
+<?php unset($_SESSION["error"]); ?>
